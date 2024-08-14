a507b3033ee2445f21ac468c7044974801775fbf
Muhammed Adhil p <167611139+adhilbathali@users.noreply.github.com>
2024-08-14 19:44:47 +0530

    add a webpage for webtoy with js

---
+++ script.js
@@ -0,0 +1,22 @@
+const gridContainer = document.querySelector('.grid-container')
+const gridTemplate = document.querySelector('.cell-template');
+const promptBtn  = document.querySelector('.prompt-btn');
+
+var n = 0;
+function prompter(){
+    gridContainer.innerHTML = '';
+    n = prompt('enter the dimension eg.10');
+    while(n>100){
+        n = prompt('Renter the dimension again. It should be less than 100');
+    }
+    for(let j = 0; j < n; j++){
+        for(let i = 0; i < n; i++){
+            const cell = gridTemplate.content.cloneNode(true).children[0];
+            const size = (100/n);
+            cell.style.width = size + 'vw';
+            cell.style.height = size + 'vw';
+            gridContainer.appendChild(cell);
+        }
+    
+    }
+}
